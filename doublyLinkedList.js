ab4cd342eed8b1ad1090e7940f94fde614e5dbd4
simon <escohen115@gmail.com>
2022-02-09 11:32:45 -0500

    shift

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,18 +1,49 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
 
 
-
-
-
-
-
-
-function shift (list){
-    if (list.head === null) return null;
-    let oldHead = list.head
-    this.head = list.head.next
-    list.head.next = null
-
-
-    return oldHead
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    shift (){
+        if (this.length === 0) return undefined;
+        if (this.length === 1){
+            this.head = null
+            this.tail = null
+            this.length = 0
+        }
+        let oldHead = this.head
+        this.head = oldHead.next
+        this.head.previous = null
+        oldHead.next = null
+        this.length--
+        return oldHead
+    }
 
 }
+        
+
+
+
+
